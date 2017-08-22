258b846c59457aee26057a4fd9e548c8a1e2a924
Cam Pedersen <cam@campedersen.com>
2017-08-22 05:00:39 +0000

    clean slate

--- lib/func.w
+++
@@ -1,8 +0,0 @@
-[= {unpack} [fn {f l} {
-  eval [join [list f] l]
-}]]
-
-[= {pack} [fn {f & xs} {f xs}]]
-
-[= {curry} unpack]
-[= {uncurry} pack]
--- src/read.h
+++
@@ -1,5 +0,0 @@
-#include "val.h"
-
-val* val_read_num(val_state* state, mpc_ast_t* t);
-val* val_read_str(val_state* state, mpc_ast_t* t);
-val* val_read(mpc_ast_t* t, char* filename);
